dc984604e7b13219162d7ea786466eccfd305c52
Zahra <mirzakizahra@gmail.com>
2021-10-02 01:39:23 -0700

    Add Department Class and It's Properties and Methods

---
+++ Department.java
@@ -0,0 +1,20 @@
+public class Department {
+    private String name;
+    private int telephonenumber;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getTelephonenumber() {
+        return telephonenumber;
+    }
+
+    public void setTelephonenumber(int telephonenumber) {
+        this.telephonenumber = telephonenumber;
+    }
+}
