95db04fb3a226ba7d8474e90e1bd521c31e2c6c4
Zahra <mirzakizahra@gmail.com>
2021-10-02 01:43:36 -0700

    Add Connection to MySQL

--- Database.java
+++ Database.java
@@ -1,5 +1,10 @@
 import java.sql.*;
 
 public class Database {
+private Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/mydb", "root", "54321");
 
+
+public Database() throws SQLException, ClassNotFoundException {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+    }
 }
